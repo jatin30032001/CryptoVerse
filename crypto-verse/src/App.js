f5872e639beb4ee2d53ff46733937f4c959d140f
jatin30032001 <jrsaini2090@gmail.com>
2022-08-09 23:49:39 +0530

    Navbar

--- crypto-verse/src/App.js
+++ crypto-verse/src/App.js
@@ -1,8 +1,9 @@
 
-import {BrowserRouter ,Route ,Routes} from "react-router-dom"
 import './App.css';
-
+import {BrowserRouter ,Route ,Routes , Link} from "react-router-dom";
+import {Layout , Typography , Space} from 'antd';
 import * as React from 'react';
+import Navbar from './Components/Navbar';
 
 
 
@@ -10,7 +11,20 @@ function App() {
   return (
 
 <div className>
-  Hell world
+ <div className="navbar">
+
+     <Navbar/>
+ </div>
+ <div className="main">
+
+ </div>
+
+
+<div className="footer">
+
+</div>
+
+
       </div>
    
 
