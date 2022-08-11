946e7080c326e44e5081f717c84570d00d68366b
jatin30032001 <jrsaini2090@gmail.com>
2022-08-12 01:15:24 +0530

    HomePage Desig

--- crypto-verse/src/App.js
+++ crypto-verse/src/App.js
@@ -48,11 +48,25 @@ function App() {
   </div>
   
 </Layout>
-       </div>
+      
 
- 
+ <div className="footer">
+   <Typography.Title level={5} style={{color : 'white' , textAlign:'center'}}>
+            Cryptoverse <br/>
+            All Rights are Reserved
+   </Typography.Title>
+
+   <Space>
+  <Link to = '/'> Home</Link> 
+  <Link to ='/exchanges'>Exchanges</Link>
+  <Link to ='/news'>News</Link>
+
+    </Space>
+
+
+ </div>
    
- 
+ </div>
 
  
     </div>
